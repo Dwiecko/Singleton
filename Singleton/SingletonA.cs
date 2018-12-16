3d306b94d82469d778802e809afd7d9bd786653f
DwieGodziny <Inalosenspace2683@gmail.com>
2018-12-16 18:46:08 +0100

    Add test and solution repository

---
+++ Singleton/SingletonA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class SingletonA <T> where T: class
+    {
+        private static T singletonInstance;
+        private static readonly object locker = new object();
+
+        protected SingletonA() { }
+
+        public static T Instance
+        {
+            get
+            {
+                if (singletonInstance == null)
+                {
+                    lock (locker)
+                    {
+                        if (singletonInstance == null)
+                        {
+                            singletonInstance = (T)Activator.CreateInstance(typeof(T), true); // create instance of type on inheritance path
+                                                                                              // is not pulic 
+                        }
+                    }
+                }
+
+                return singletonInstance;
+            }
+        }
+
+    }
+}
---
+++ SingletonThreadSafetyTests/InheritanceTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton;
+
+namespace SingletonThreadSafetyTests
+{
+    [TestClass]
+    public class InheritanceTests
+    {
+        [TestMethod]
+        [TestCategory("Inherience - same")]
+        public void Test_Inherence_Of_Childrens()
+        { 
+            SingletonA<SingletonB> firstSon = SingletonB.Instance;
+            SingletonA<SingletonB> secondSon = SingletonB.Instance;
+
+            SingletonA<SingletonC> firstDaughter = SingletonC.Instance;
+            SingletonA<SingletonC> secondDaughter = SingletonC.Instance;
+
+            Assert.AreSame(firstSon, secondSon);
+            Assert.AreSame(firstDaughter, secondDaughter);
+        }
+        [TestMethod]
+        [TestCategory("Inherience - not same")]
+        public void Test_Inherence_OfChildrens_ChildrensAre_Not_TheSame()
+        {
+            SingletonA<SingletonB> firstSon = SingletonB.Instance;
+            SingletonA<SingletonB> secondSon = SingletonB.Instance;
+
+            SingletonA<SingletonC> firstDaughter = SingletonC.Instance;
+            SingletonA<SingletonC> secondDaughter = SingletonC.Instance;
+
+            Assert.AreNotSame(firstSon, firstDaughter);
+        }
+    }
+}
